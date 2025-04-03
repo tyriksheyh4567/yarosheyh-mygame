f1a84cfa9f263a38c9f6315f3a3c7e2906f8ae10
Yaromir Sheydaev <tyrikaltsheyhog@gmail.com>
2025-04-03 12:05:12 +0700

    lslslslslslslslslsls

--- app/app.vue
+++ app/app.vue
@@ -6,15 +6,11 @@
   </UApp>
 </template>
 
-<script setup>
-import { UApp } from '#components';
-import { ref } from 'vue';
-import Loading from '~/components/Loading.vue';
-
-const isLoading = ref(true);
-
-// Simulate loading state for demonstration
-setTimeout(() => {
-  isLoading.value = false; // Hide loading after 3 seconds
-}, 3000);
+<script setup lang="ts">
+useHead({
+  title: 'Своя Игра',
+  meta: [
+    { name: 'description', content: 'Своя игра на Nuxt 4.' }
+  ],
+})
 </script>
